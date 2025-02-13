e29a72ee19e7e1613dc1e970d3452959310c3d59
Jonas.Heden <heden.jonas@gmail.com>
2025-02-13 19:35:14 +0100

    Add project files.

---
+++ Data/Entites/UserEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Entites;
+
+public class UserEntity
+{
+    [Key]
+    public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+}
